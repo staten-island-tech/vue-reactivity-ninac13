cc834019ed6e82bef0f0a5c6eef9b659873bab5f
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-01-27 19:16:37 -0500

    Revert "117"
    This reverts commit c926ea2f94141df07116c03cffb8fe0fc105e0b9.

--- app/src/main.js
+++ app/src/main.js
@@ -3,11 +3,9 @@ import './assets/main.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import { jewelryArray } from '/src/products.js';
 
 const app = createApp(App)
 
 app.use(router)
 
 app.mount('#app')
-
