3680265cafb3571a5ad3ba98651198a2f7ef3ec0
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-01-27 19:55:04 -0500

    images 1/27

---
+++ app/src/products.js
@@ -0,0 +1,182 @@
+const jewelryArray = [
+    {
+      name: "Gold Necklace Set",
+      material: "Gold",
+      price: 350,
+      category: "Necklace",
+      brand: "Luxora",
+      availability: "In Stock",
+      img: "img/goldnecklaceset.jpg"
+    },
+    {
+      name: "Diamond Ring",
+      material: "Diamond",
+      price: 1200,
+      category: "Ring",
+      brand: "Sparkle",
+      availability: "Out of Stock",
+      img: "img/diamonring.jpg"
+    },
+    {
+      name: "Silver Bracelet",
+      material: "Silver",
+      price: 85,
+      category: "Bracelet",
+      brand: "ShinySilver",
+      availability: "In Stock",
+      img: "img/silverbracelet.jpg"
+    },
+    {
+      name: "Pearl Earrings",
+      material: "Pearl, Gold",
+      price: 500,
+      category: "Earrings",
+      brand: "Elegance",
+      availability: "In Stock",
+      img: "img/pearlearrings.jpg"
+    },
+    {
+      name: "Ruby Pendant",
+      material: "Ruby, Silver",
+      price: 700,
+      category: "Pendant",
+      brand: "GemGlow",
+      availability: "In Stock",
+      img: "img/rubypendant.jpg"
+    },
+    {
+      name: "Sapphire Brooch",
+      material: "Sapphire, Gold",
+      price: 850,
+      category: "Brooch",
+      brand: "RoyalGems",
+      availability: "Out of Stock",
+      img:"img/sapphirebrooch.jpg"
+    },
+    {
+      name: "Titanium Watch",
+      material: "Titanium",
+      price: 250,
+      category: "Watch",
+      brand: "TimeMaster",
+      availability: "In Stock",
+      img:"img/watch.jpg"
+    },
+    {
+      name: "Rose Gold Bangle",
+      material: "Rose Gold",
+      price: 400,
+      category: "Bracelet",
+      brand: "RoseTide",
+      availability: "In Stock",
+      img:"img/bangle.jpg"
+    },
+    {
+      name: "Emerald Ring",
+      material: "Emerald, Gold",
+      price: 1500,
+      category: "Ring",
+      brand: "LuxGem",
+      availability: "In Stock",
+      img:"img/emeraldring.jpg"
+    },
+    {
+      name: "Platinum Watch",
+      material: "Platinum",
+      price: 600,
+      category: "Watch",
+      brand: "Elegance",
+      availability: "In Stock",
+      img:"img/platinumwatch.jpg"
+    },
+    {
+      name: "Diamond Stud Earrings",
+      material: "Diamond, Gold",
+      price: 600,
+      category: "Earrings",
+      brand: "Brilliant",
+      availability: "In Stock",
+      img:"img/diamondearrings.jpg"
+    },
+    {
+      name: "Opal Pendant",
+      material: "Opal, Silver",
+      price: 300,
+      category: "Pendant",
+      brand: "OpalCraft",
+      availability: "Out of Stock",
+      img:"img/pendant.jpg"
+    },
+    {
+      name: "Leather Bracelet",
+      material: "Leather, Metal",
+      price: 40,
+      category: "Bracelet",
+      brand: "UrbanStyle",
+      availability: "In Stock",
+      img:"img/leather.jpg"
+    },
+    {
+      name: "Aquamarine Necklace",
+      material: "Aquamarine, Silver",
+      price: 450,
+      category: "Necklace",
+      brand: "OceanGlow",
+      availability: "In Stock",
+      img:"img/necklace.jpg"
+    },
+    {
+      name: "Vintage Brooch",
+      material: "Gold, Enamel",
+      price: 350,
+      category: "Brooch",
+      brand: "ClassicGems",
+      availability: "In Stock",
+      img:"img/goldbrooch.jpg"
+    },
+    {
+      name: "Gold Chain",
+      material: "Gold",
+      price: 220,
+      category: "Necklace",
+      brand: "GoldEssence",
+      availability: "Out of Stock",
+      img:"img/goldchain.jpg"
+    },
+    {
+      name: "Silver Chunky Ring",
+      material: "Silver",
+      price: 30,
+      category: "Ring",
+      brand: "SilverLuxe",
+      availability: "In Stock",
+      img:"img/silverring.jpg"
+    },
+    {
+      name: "Amethyst Ring",
+      material: "Amethyst, Gold",
+      price: 550,
+      category: "Ring",
+      brand: "GemStone",
+      availability: "In Stock",
+      img:"img/amethystring.jpg"
+    },
+    {
+      name: "Pearl Necklace",
+      material: "Pearl, Silver",
+      price: 800,
+      category: "Necklace",
+      brand: "PearlGrace",
+      availability: "In Stock",
+      img:"img/silverpearl.jpg"
+    },
+    {
+      name: "Charm Bracelet",
+      material: "Stainless Steel",
+      price: 60,
+      category: "Bracelet",
+      brand: "SteelCharm",
+      availability: "Out of Stock",
+      img:"img/charmbracelet.jpg"
+    }
+  ];
