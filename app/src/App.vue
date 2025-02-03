c8e24daa0bff0eb6f7ca6f14da6feecc70d82c0c
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-02-03 14:00:01 -0500

    2/3

--- app/src/App.vue
+++ app/src/App.vue
@@ -1,21 +1,12 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
 </script>
 
 <template>
   <header>
     <div class="wrapper">
-      <HelloWorld msg="Jewelry Store" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+      <h1>Jewelry Store</h1>
     </div>
   </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
---
+++ app/src/components/Headings.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+  <header>
+    <div class="wrapper">
+      <h1>Jewelry Store</h1>
+    </div>
+  </header>
+</template>
