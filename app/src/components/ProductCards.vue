9eff95677aa3ccfd35e54c7ecd2561b360b28488
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-02-13 14:03:08 -0500

    2/13

--- app/src/components/ProductCards.vue
+++ app/src/components/ProductCards.vue
@@ -10,13 +10,13 @@ defineProps({
 </script>
 
 <template>
-    <div class="card w-2/5 pb-50 pt-12 h-auto border-4 border-secondary rounded-lg border-double mx-5 my-5 min-w-64 shadow-md bg-base-300">
+    <div class="card w-2/5 pb-50 pt-6 h-auto border-4 border-secondary rounded-lg border-double mx-5 my-5 min-w-64 shadow-md bg-base-100">
         <h2 class="text-center">{{ category }}</h2>
-        <h1 class="text-center">{{ name }}</h1>
+        <h1 class="text-center text-2xl text-neutral">{{ name }}</h1>
         <h2 class="text-center">Brand: {{ brand }}</h2>
         <img :src="img" class="rounded-lg border-double border-4 border-base-300 w-full h-96 object-cover"/> 
         <h2 class="text-center">${{ price }}</h2>
-        <h2 class="text-center">Material(s): {{ material }}</h2>
+        <h2 class="text-center pb-2">Material(s): {{ material }}</h2>
         <slot></slot>
     </div>
 </template>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -7,8 +7,8 @@ const jewelryProducts = ref(jewelryArray);
 const cart = ref([]);
 </script>
 
-<template class="bg-neutral-content">
-  <main>
+<template>
+  <main class="bg-neutral-content">
     <h1 class="justify-self-start font-shrikhand flex justify-center text-base-content text-5xl mx-auto w-fit px-4 py-4 bg-base-100 border-neutral border-4 mt-7 mb-5 underline underline-offset-8 rounded-3xl shadow-2xl">Jewelry Store!</h1>
     <div class="grid grid-cols-5 gap-4">
       <div class="col-span-3 flex-1 ml-60 container flex justify-center flex-wrap">
@@ -20,7 +20,7 @@ const cart = ref([]);
           :img="jewelry.img"
           :price="jewelry.price"
           :material="jewelry.material"
-        ><button @click="console.log(jewelry.name)" class="border-4 rounded-lg border-base-100 bg-neutral-content hover:bg-base-100 duration-500">Add to Cart</button></Products>
+        ><button @click="console.log(jewelry.name)" class="border-4 rounded-lg border-base-100 pt-2 pb-1 bg-neutral-content hover:bg-base-100 hover:text-neutral hover:text-xl duration-500">Add to Cart</button></Products>
       </div>
       <div class="col-span-1 h-full absolute right-10 border-4 border-neutral justify-self-end px-40 rounded-lg">
         <h1 class="text-2xl pt-5 text-center">Jewelry Cart</h1>
