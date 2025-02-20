01638d37773d00161b4e5864936cbeb7ba94424a
ninac13 <144480538+ninac13@users.noreply.github.com>
2025-02-19 23:04:21 -0500

    2/19

--- app/src/components/ShoppingCart.vue
+++ app/src/components/ShoppingCart.vue
@@ -8,11 +8,11 @@ defineProps({
 
 <template>
     <div>
-        <h1 class="text-center">{{ name }}</h1>
-        <div class="flex justify-center">
+        <h1 class="text-center mt-3 text-neutral text-lg mb-2">{{ name }}</h1>
+        <div class="flex justify-center mb-3">
             <img :src="img" class="w-2/5"/>
         </div>
-        <h3 class="text-center text-sm">${{ price }}</h3>
+        <h3 class="text-center text-sm mb-2">${{ price }}</h3>
         <div class="flex justify-center">
             <slot></slot>
         </div>
--- app/src/views/HomeView.vue
+++ app/src/views/HomeView.vue
@@ -5,6 +5,7 @@ import jewelryArray from "../products.js";
 import { ref } from 'vue';
 const jewelryProducts = ref(jewelryArray);
 const cart = ref([]);
+const totalAmount = ref(0);
 </script>
 
 <template>
@@ -20,21 +21,24 @@ const cart = ref([]);
           :img="jewelry.img"
           :price="jewelry.price"
           :material="jewelry.material"
-        ><button @click="cart.push(jewelry)" class="border-base-100 pt-2 pb-1 bg-neutral-content hover:bg-base-100 hover:text-neutral hover:text-xl duration-500 border-8 rounded-2xl">Add to Cart</button></Products>
+        ><button @click="cart.push(jewelry); totalAmount+=jewelry.price" class="border-base-100 pt-2 pb-1 bg-neutral-content hover:bg-base-100 hover:text-neutral hover:text-xl duration-500 border-8 rounded-2xl">Add to Cart</button></Products>
       </div>
       <div class="fixed col-span-1 h-5/6 w-1/4 border-8 bg-base-100 border-double border-neutral justify-self-end mr-16 rounded-lg overflow-auto">
-        <h1 class="text-2xl pt-5 text-center flex justify-center flex-wrap">Jewelry Cart</h1>
+        <h1 class="text-2xl pt-5 text-center flex justify-center flex-wrap text-neutral">Jewelry Cart</h1>
+        <h1 class="text-center mt-3">Total Cost: ${{ totalAmount }}</h1>
         <div class="flex justify-center">
-          <button @click="cart=[]" class="border-base-100 pt-2 pb-1 bg-neutral-content hover:bg-base-100 hover:text-neutral hover:text-xl duration-500 border-8 rounded-2xl w-2/3">Remove All Items</button>
+          <button @click="cart=[]; totalAmount=0;" class="border-base-100 pt-2 pb-1 bg-neutral-content hover:bg-base-100 hover:text-neutral hover:text-xl duration-500 border-8 rounded-2xl w-2/3">Remove All Items</button>
         </div>
-        <div class="container flex justify-center flex-wrap bg-neutral-content rounded-xl">
-          <Cart v-for="item in cart"
-            :key="item.name"
-            :name="item.name"
-            :img="item.img"
-            :price="item.price"
-          ><button @click="cart=cart.filter(cartItem => cartItem!==item)" class="mb-5 border-base-100 pt-2 pb-1 bg-neutral-content hover:bg-base-100 hover:text-neutral hover:text-base duration-500 border-8 rounded-2xl w-1/2 text-sm">Remove Item(s)</button></Cart>
-        </div>
+        <div class="flex justify-center ">
+          <div class="container flex justify-center flex-wrap bg-neutral-content rounded-xl w-72">
+            <Cart v-for="item in cart"
+              :key="item.name"
+              :name="item.name"
+              :img="item.img"
+              :price="item.price"
+            ><button @click="totalAmount-=item.price*(cart.filter(cartItem => cartItem === item).length); cart=cart.filter(cartItem => cartItem!==item);" class="mb-5 pt-2 pb-1 bg-base-100 hover:bg-neutral-content hover:text-neutral hover:text-base duration-500 rounded-2xl w-1/2 text-sm">Remove {{ item.name }}</button></Cart>
+          </div>
+      </div>
       </div>
     </div>
   </main>
